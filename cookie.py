a3570ae6d4766a6ab0d3aa87fe27ac866ab06dae
Nikita <151358720+NikuiTka@users.noreply.github.com>
2025-01-26 12:54:54 +0200

    example
    Easy example code, how work cookie files.

---
+++ cookie.py
@@ -0,0 +1,18 @@
+# How works cookie (easy example):
+# Scanning user data
+username = input("Username: ")
+password  = input("Password: ")
+# Infinity commands logic
+while True:
+  # Scanner for commands
+    cmd = input("Enter Command: ")
+  #logic (for check username)
+    if cmd == "username":
+        print(username)
+    elif cmd == "password":
+      #logic (for check password)
+        print(password)
+      #logic (for exit)
+    elif cmd == "quit":
+        break
+# This is easy example, how working cookie files.
